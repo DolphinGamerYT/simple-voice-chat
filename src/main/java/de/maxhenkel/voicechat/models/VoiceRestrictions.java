340d9bff3528b2bcbbbabfc31d1bce5ec4f5cc1b
DolphinGamerYT <dolphingamerytof@gmail.com>
2022-01-04 17:43:42 -0800

    Created VoiceRestrictions

---
+++ src/main/java/de/maxhenkel/voicechat/models/VoiceRestrictions.java
@@ -0,0 +1,62 @@
+package de.maxhenkel.voicechat.models;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public class VoiceRestrictions {
+
+    private final String permission;
+    private final List<UUID> mutedPlayers;
+    private boolean allMuted;
+    private final List<UUID> speakers;
+
+    public VoiceRestrictions() {
+        this.permission = "squidvoice.bypass";
+        this.mutedPlayers = Collections.synchronizedList(new ArrayList<>());
+        this.allMuted = false;
+        this.speakers = Collections.synchronizedList(new ArrayList<>());
+    }
+
+    public boolean checkPlayer(Player player) {
+        if (player.hasPermission(this.permission)) return true;
+        if (this.allMuted) return false;
+        if (this.mutedPlayers.contains(player.getUniqueId())) return false;
+        return true;
+    }
+
+    public boolean isSpeaker(Player player) {
+        return this.speakers.contains(player.getUniqueId());
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public boolean isAllMuted() {
+        return allMuted;
+    }
+
+    public void setAllMuted(boolean allMuted) {
+        this.allMuted = allMuted;
+    }
+
+    public void addMutedPlayer(UUID uuid) {
+        this.mutedPlayers.add(uuid);
+    }
+
+    public void removeMutedPlayer(UUID uuid) {
+        this.mutedPlayers.remove(uuid);
+    }
+
+    public void addSpeaker(UUID uuid) {
+        this.speakers.add(uuid);
+    }
+
+    public void removeSpeaker(UUID uuid) {
+        this.speakers.remove(uuid);
+    }
+}
